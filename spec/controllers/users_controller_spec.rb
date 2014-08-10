903b4386f281db37dac68df87705cf263c70ea3b
Cullen Jett <cullenjett@gmail.com>
2014-08-10 15:40:40 -0600

    Add flash messages to layout, start users controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 describe UsersController do
-  
+  describe "POST create" do
+    context "successful signup" do
+      it "sets the flash[:success] message" do
+        post :create, user: { first_name: "John", last_name: "Doe", email: "test@example.com", password: "password" }
+        expect(flash[:success]).to be_present
+      end
+
+      it "redirects to the user show page"
+    end
+
+    context "unsuccessful signup" do
+
+    end
+  end
 end
