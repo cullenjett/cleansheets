0cf77311d4b525df38f7b8f567a3283ef2036313
Cullen Jett <cullenjett@gmail.com>
2014-08-10 15:27:34 -0600

    Configure RSpec, add User spec

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
 end
 
 group :test do
-  gem 'capybara'
   gem 'shoulda-matchers'
+  gem 'capybara'
+  gem 'database_cleaner'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User
   field :last_name, type: String
   field :email, type: String
   field :password, type: String
+
+  validates_presence_of :first_name, :last_name, :email, :password
+  validates_uniqueness_of :email
 end
